ab648c851cd53143d003671e3c4a66b7d66e913e
dmytro <testtestdv@protonmail.com>
2020-11-19 15:50:42 +0200

    Api other component

--- src/Components/Users/UsersContainer.jsx
+++ src/Components/Users/UsersContainer.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import Axios from 'axios';
 import { connect } from 'react-redux';
 import { toggleFolllowAC, setUsersAC, setTotalUsersAC, setPageAC } from '../../redux/usersReducer';
 import Users from './Users';
+
+class UsersAPIContainer extends React.Component {
+
+    componentDidMount() {
+        Axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.pageNumber}&count=10`).then(response => {
+            console.log(response);
+            this.props.setUsers(response.data.items);
+            this.props.setTotalCount(response.data.totalCount);
+        })
+    }
+
+    setPageNumber = (page) => {
+        this.props.setPage(page);
+        Axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=10`).then(response => {
+            console.log(response);
+            this.props.setUsers(response.data.items);
+            this.props.setTotalCount(response.data.totalCount);
+        })
+    }
+
+    render() {
+
+        return (
+            <Users pageNumber={this.props.pageNumber} users={this.props.users} toggleFollow={this.props.toggleFolllow} pagesCount={this.props.pagesCount} setPageNumber={this.setPageNumber}/>
+        )
+    }
+}
 
 let mapStateToProps = (state) => {
     return state.usersReducer    
@@ -24,6 +52,6 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersAPIContainer);
 
 export default UsersContainer;
